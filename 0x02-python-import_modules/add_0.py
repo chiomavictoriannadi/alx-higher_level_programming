54286a6ceada80f9a833b43408658fa6d0813dff
chiomavictoriannadi <chiomavictoriannadi@gmail.com>
2023-06-08 21:36:32 -0700

    Update

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My additional function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The value of a + b is returned
+    """
+    return (a + b)
